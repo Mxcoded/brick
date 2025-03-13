24ce50b46014219c351e8dda345e539de15698a6
Oluwasheyi Makanjuola <jaywoncoded@gmail.com>
2025-03-13 17:05:10 +0100

    Added Maintenance Module

---
+++ Modules/Maintenance/app/Http/Controllers/Controller.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Modules\Staff\Http\Controllers;
+
+use Illuminate\Routing\Controller as BaseController;
+
+class Controller extends BaseController
+{
+    // Add any shared logic for your module controllers here
+}
