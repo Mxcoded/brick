a8eedfe0f93369347f34187e27eca396c7284392
Oluwasheyi Makanjuola <jaywoncoded@gmail.com>
2025-03-21 23:54:19 +0100

    added laravel/ui

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Auth;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home   ', [HomeController::class, 'index'])->name('home');
