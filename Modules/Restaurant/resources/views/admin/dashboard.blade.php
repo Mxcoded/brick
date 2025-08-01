6dfb5dbee901a66793cbb13de505315a4f7dc437
Oluwasheyi Makanjuola <jaywoncoded@gmail.com>
2025-08-01 18:04:51 +0100

    rename button

--- Modules/Restaurant/resources/views/admin/dashboard.blade.php
+++ Modules/Restaurant/resources/views/admin/dashboard.blade.php
@@ -5,7 +5,7 @@
         <h1 class="mb-4">Add Menu Category</h1>
         <div class="card shadow-sm">
             <div class="card-body">
-                @if(session('success'))
+                @if (session('success'))
                     <div class="alert alert-success">
                         {{ session('success') }}
                     </div>
@@ -15,10 +15,10 @@
                     <div class="row g-3">
                         <div class="col-md-4">
                             <label for="parent_category" class="form-label">Menu Category</label>
-                            <select name="parent_category" id="parent_category" class="form-select" >
+                            <select name="parent_category" id="parent_category" class="form-select">
                                 <option value="" disabled selected>Select Menu Categories</option>
-                                @foreach($parent_categories as $category)
-                                    <option value="{{$category->id}}">{{$category->name}}</option>
+                                @foreach ($parent_categories as $category)
+                                    <option value="{{ $category->id }}">{{ $category->name }}</option>
                                 @endforeach
                             </select>
                             @error('parent_category')
@@ -27,7 +27,7 @@
                         </div>
                         <div class="col-md-4">
                             <label for="sub_category" class="form-label">Sub category</label>
-                            <select name="sub_category" id="sub_category" class="form-select" >
+                            <select name="sub_category" id="sub_category" class="form-select">
                                 <option value="" disabled selected>Select Sub Menu Categories</option>
                             </select>
                             @error('sub_category')
@@ -38,12 +38,17 @@
                             <label for="name" class="form-label">Category Name</label>
                             <input type="text" name="name" id="name" class="form-control" required>
                         </div>
-                        
+
                     </div>
-                    <div class="mt-4">
-                        <button type="submit" class="btn btn-primary">Submit Balance</button>
-                        <a href="{{ route('staff.leaves.index') }}" class="btn btn-secondary">Cancel</a>
+                    <div class="d-flex flex-wrap gap-2 mt-4">
+                        <button type="submit" class="btn btn-primary d-flex align-items-center">
+                            Save <i class="fas fa-save ms-2"></i>
+                        </button>
+                        <a href="{{ route('restaurant.landing') }}" class="btn btn-secondary">
+                            Cancel <i class="fas fa-times ms-2"></i>
+                        </a>
                     </div>
+
                 </form>
             </div>
         </div>
@@ -51,16 +56,17 @@
 @endsection
 
 @section('scripts')
-    <script >
+    <script>
         $(document).ready(function() {
-            document.getElementById('parent_category').addEventListener('change', function () {
-                const selectedValue = event.target.value; 
+            document.getElementById('parent_category').addEventListener('change', function() {
+                const selectedValue = event.target.value;
                 console.log('Selected Parent Category ID:', selectedValue);
                 var categories = @json($categories); // For numbers (without quotes)
                 // Filter categories based on the selected parent category
                 var sub_categories = categories.filter(category => category.parent_id == selectedValue);
-                document.getElementById('sub_category').innerHTML = '<option value="" >Select Sub Menu Categories</option>';
-                sub_categories.forEach(function (category) {
+                document.getElementById('sub_category').innerHTML =
+                    '<option value="" >Select Sub Menu Categories</option>';
+                sub_categories.forEach(function(category) {
                     var option = document.createElement('option');
                     option.value = category.id;
                     option.textContent = category.name;
@@ -71,4 +77,3 @@
         });
     </script>
 @endsection
-
