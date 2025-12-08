9d4541009fd001235080e555a553214f3e89f309
Oluwasheyi Makanjuola <jaywoncoded@gmail.com>
2025-12-08 01:16:11 +0100

    fixing sidebar

--- Modules/Frontdeskcrm/resources/views/layouts/menu.blade.php
+++ Modules/Frontdeskcrm/resources/views/layouts/menu.blade.php
@@ -1,4 +1,4 @@
-<a href="{{ route('frontdesk.dashboard') }}" class="flex items-center px-4 py-2 text-sm font-medium rounded-md hover:bg-gray-800 text-gray-300 {{ request()->routeIs('frontdesk.dashboard') ? 'bg-gray-800 text-white' : '' }}">
+<a href="{{ route('frontdesk.registrations.dashboard') }}" class="flex items-center px-4 py-2 text-sm font-medium rounded-md hover:bg-gray-800 text-gray-300 {{ request()->routeIs('frontdesk.registrations.dashboard') ? 'bg-gray-800 text-white' : '' }}">
     <i class="fas fa-concierge-bell w-6 text-center mr-2"></i> Overview
 </a>
 <a href="{{ route('frontdesk.registrations.create') }}" class="flex items-center px-4 py-2 text-sm font-medium rounded-md hover:bg-gray-800 text-gray-300 {{ request()->routeIs('frontdesk.registrations.create') ? 'bg-gray-800 text-white' : '' }}">
