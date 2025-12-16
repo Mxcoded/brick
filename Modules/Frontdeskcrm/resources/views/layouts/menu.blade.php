c248cb3ad3db972d7a2be2cc028146b3f8986f1e
Oluwasheyi Makanjuola <jaywoncoded@gmail.com>
2025-12-17 00:13:55 +0100

    Fix menus and routes

--- Modules/Frontdeskcrm/resources/views/layouts/menu.blade.php
+++ Modules/Frontdeskcrm/resources/views/layouts/menu.blade.php
@@ -7,13 +7,12 @@
     <i class="fas fa-chevron-down small"></i>
 </a>
 <div class="collapse {{ request()->routeIs('frontdesk.*') ? 'show' : '' }}" id="frontdeskSubmenu">
-    <a href="{{ route('frontdesk.registrations.dashboard') }}" class="list-group-item list-group-item-action {{ request()->routeIs('frontdesk.registrations.dashboard') ? 'active' : '' }}" style="color: #ddd; border: none;">Overview</a>
+    <a href="{{ route('frontdesk.registrations.dashboard') }}" class="list-group-item list-group-item-action {{ request()->routeIs('frontdesk.registrations.dashboard') ? 'active' : '' }}" style="color: #ddd; border: none;">Guest List</a>
     
     @can('check_in_guest')
     <a href="{{ route('frontdesk.registrations.create') }}" class="list-group-item list-group-item-action {{ request()->routeIs('frontdesk.registrations.create') ? 'active' : '' }}" style="color: #ddd; border: none;">Check In</a>
     <a href="{{ route('frontdesk.registrations.createWalkin') }}" class="list-group-item list-group-item-action {{ request()->routeIs('frontdesk.registrations.createWalkin') ? 'active' : '' }}" style="color: #ddd; border: none;">Walk-in</a>
     @endcan
     
-    <a href="{{ route('frontdesk.registrations.index') }}" class="list-group-item list-group-item-action {{ request()->routeIs('frontdesk.registrations.index') ? 'active' : '' }}" style="color: #ddd; border: none;">Guest List</a>
 </div>
 @endcan
