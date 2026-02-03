ca82457266c5abab18f3816c3b2487409e27fd84
Oluwasheyi Makanjuola <jaywoncoded@gmail.com>
2026-02-03 18:40:23 +0100

    fix add phone to registration

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -38,6 +38,19 @@
                                 @enderror
                             </div>
                         </div>
+                         <div class="row mb-3">
+                            <label for="contact_number" class="col-md-4 col-form-label text-md-end">{{ __('Contact Number') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="contact_number" type="text" class="form-control @error('contact_number') is-invalid @enderror" name="contact_number" value="{{ old('contact_number') }}" required autocomplete="contact_number">
+
+                                @error('contact_number')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
 
                         <div class="row mb-3">
                             <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
