c8f1e64a2c0bfe299e5706e13fc46eff8ceb0399
Oluwasheyi Makanjuola <jaywoncoded@gmail.com>
2026-02-03 18:45:50 +0100

    fix validation for phone number

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -30,7 +30,7 @@ class RegisterController extends Controller
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'contact_number' => ['nullable', 'string', 'max:191', 'unique:modules_frontdeskcrm_guests,contact_number'],
+            'contact_number' => ['required', 'string', 'max:191', 'unique:guest,contact_number'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
     }
