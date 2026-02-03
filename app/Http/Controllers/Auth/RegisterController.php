4ccc6bc5ab98db2cfbaec88c0cf886a86bfa60fc
Oluwasheyi Makanjuola <jaywoncoded@gmail.com>
2026-02-03 18:48:53 +0100

    add s to guests validation

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -30,7 +30,7 @@ class RegisterController extends Controller
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'contact_number' => ['required', 'string', 'max:191', 'unique:guest,contact_number'],
+            'contact_number' => ['required', 'string', 'max:191', 'unique:guests,contact_number'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
     }
