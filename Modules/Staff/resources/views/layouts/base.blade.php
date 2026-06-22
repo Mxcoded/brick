4066b464e20bced657d665884c1c706f1145f4d9
Oluwasheyi Makanjuola <jaywoncoded@gmail.com>
2026-06-22 16:06:10 +0100

    fix the vite issue for sidebar

--- Modules/Staff/resources/views/layouts/base.blade.php
+++ Modules/Staff/resources/views/layouts/base.blade.php
@@ -14,7 +14,7 @@
     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap5.min.css">
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-    <link rel="stylesheet" href="{{ asset('css/sidebar.css') }}">
+
 
     @yield('styles')
 </head>
--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -16,7 +16,6 @@
     <!-- Vite (Bootstrap, Icons, FontAwesome) -->
     @vite(['resources/sass/app.scss'])
     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap5.min.css">
-    <link rel="stylesheet" href="{{ asset('css/sidebar.css') }}">
 
     <style>
         :root {
